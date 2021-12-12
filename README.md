e0a06ba3da667bba59f5cdb9dcb5a19484476ab1
Cjacker <jianzhong.huang@i-soft.com.cn>
2021-12-13 02:29:27 +0800

    minor fixes

--- README.md
+++ README.md
@@ -3,10 +3,10 @@ Additional cores for miyoo mini.
 
 miyoo mini is a mini handsheild made in China and support retroarch.
 
-The default cores miyoo mini provided is lack of atari2600 (stella2014)/pokemini/snes9x (instead, provide snes9x2010).
+The default retroarch cores that miyoo mini provided is lack of atari800/atari2600 (stella2014)/pokemini/snes9x (instead, provide snes9x2010).
 
-Here is some cores for miyoo mini retroarch.
+Here is these cores for miyoo mini retroarch.
 
 
 # Installation
-copy to 'RApp' dir and create 'atari2600'/'snes9x'/'pokemini' dir under 'RRoms'.
+copy core dirs to 'RApp' dir and put games to 'atari800'/'atari2600'/'snes9x'/'pokemini' dir under 'RRoms'.
--- atari800/launch.sh
+++ atari800/launch.sh
@@ -3,14 +3,18 @@ echo $0 $*
 progdir=`dirname "$0"`
 homedir=`dirname "$1"`
 
+# create game rom dir.
+mkdir -p /mnt/SDCARD/RRoms/atari800
+
 # input remap to Atari joystick 
 mkdir -p /mnt/SDCARD/RetroArch/.retroarch/config/remaps/Atari800/
 cp -rf $progdir/Atari800.rmp /mnt/SDCARD/RetroArch/.retroarch/config/remaps/Atari800/Atari800.rmp
+
 # core config file
 mkdir -p /mnt/SDCARD/RetroArch/.retroarch/config/Atari800/
 cp -rf $progdir/Atari800.opt /mnt/SDCARD/RetroArch/.retroarch/config/Atari800/
 
-# if 
+# if bios not exists, copy it.
 if [ ! -f /mnt/SDCARD/RetroArch/.retroarch/system/5200.rom ]; then
   cp -rf $progdir/bios/5200.rom /mnt/SDCARD/RetroArch/.retroarch/system/5200.rom
 fi
